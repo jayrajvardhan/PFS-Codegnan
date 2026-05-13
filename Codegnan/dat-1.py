5c629cdd52140569861663faa3b7ef6a6da84607
jayrajvardhan <jayrajvardhanp@gmail.com>
2026-05-13 15:38:02 +0530

    Add files via upload
    Day 1 of Learning Python

---
+++ Codegnan/dat-1.py
@@ -0,0 +1,30 @@
+'''print("This is my first python class")
+
+
+
+
+
+
+n = 35
+so = "python"
+any = [1,2]
+an = {"Name" : "jay"}
+print(n)
+print(so)
+print(any)
+print(an)
+
+
+
+'''
+a = {}
+b = {}
+c = {}
+
+
+Codegnan_VSP_Stu_Details = {"Name" : "jay"}
+Codegnan_HYD_Stu_Details = {"Name" : "Arjun"}
+Codegnan_VIJ_Stu_Details = {"Name" : "Prakash"}
+
+print(Codegnan_HYD_Stu_Details)
+print(Codegnan_VSP_Stu_Details)
